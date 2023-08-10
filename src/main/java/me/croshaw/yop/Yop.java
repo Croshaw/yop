f27fb062c2842ea5e5420c2d0ecd23ce1a428bc0
Croshaw <crosafixx200215@gmail.com>
2023-08-10 21:59:49 +0500

    init commit

---
+++ src/main/java/me/croshaw/yop/Yop.java
@@ -0,0 +1,10 @@
+package me.croshaw.yop;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Yop implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/me/croshaw/yop/client/YopClient.java
@@ -0,0 +1,10 @@
+package me.croshaw.yop.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class YopClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
